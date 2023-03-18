232b234bb0eec7acb92c231abec5a83ae2a5da4c
Chanwit <chanwitt@gmail.com>
2023-03-18 18:03:18 +0700

    add cert

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const express = require('express');
+
 const cors = require('cors');
-const app = express();
+
 const swaggerUi = require('swagger-ui-express');
 const os = require('os');
 const axios = require('axios')
 var morgan = require('morgan')
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+
+var privateKey  = fs.readFileSync('cert/mylab.local.key', 'utf8');
+var certificate = fs.readFileSync('cert/mylab.local.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+const express = require('express');
+const app = express();
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+
+
+
 swaggerJsdoc = require("swagger-jsdoc")
 // const swaggerDocument = require('./swagger.json');
 app.use(express.json({ limit: '50mb' }));
@@ -13,7 +32,7 @@ const port = 4000;
 app.use(cors());
 app.use(morgan('dev'));
 //import json file
-const fs = require('fs');
+
 let rawdata = fs.readFileSync('data.json');
 let products = JSON.parse(rawdata);
 
@@ -117,6 +136,15 @@ app.get('/f5', async (req, res) => {
     res.json(products)
 })
 
+app.get('/loadtest', async (req, res) => {
+    fs.readFile('load-test.json', (err, data) => {
+        if (err) throw err;
+        let results = JSON.parse(data);
+        res.send(results)
+    });
+    
+})
+
 app.post('/compare', (req, res) => {
     console.log(req.body)
     const num1 = req.body.num1
@@ -132,8 +160,9 @@ app.post('/compare', (req, res) => {
 })
 
 
-
-
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+httpServer.listen(8080, () => {
+    console.log(`Listening at http://localhost:8080`);
+});
+httpsServer.listen(8443, () => {
+    console.log(`Listening at http://localhost:8443`);
 });
