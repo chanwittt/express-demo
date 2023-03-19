ab9a2b84ee4d411f608b0b70c0e1d674ad8be6b6
Chanwit <chanwitt@gmail.com>
2023-03-19 09:31:30 +0700

    ลบ endpoint

--- app.js
+++ app.js
@@ -22,10 +22,6 @@ var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 
-
-
-swaggerJsdoc = require("swagger-jsdoc")
-// const swaggerDocument = require('./swagger.json');
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 const port = 4000;
@@ -36,102 +32,10 @@ app.use(morgan('dev'));
 let rawdata = fs.readFileSync('data.json');
 let products = JSON.parse(rawdata);
 
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Express API with Swagger",
-            version: "0.1.0-beta",
-            description:
-                "This is a simple CRUD API application made with Express and documented with Swagger",
-            // license: {
-            //     name: "MIT",
-            //     url: "https://spdx.org/licenses/MIT.html",
-            // },
-            // contact: {
-            //     name: "LogRocket",
-            //     url: "https://logrocket.com",
-            //     email: "info@email.com",
-            // },
-        },
-    },
-    apis: ["app.js"],
-};
-
-const specs = swaggerJsdoc(options);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
-
-/**
- * @swagger
- * /headers:
- *   get:
- *     description: Get all data from request headers
- *     responses:
- *       200:
- *         description: Success
- * 
- */
-app.get('/headers', (req, res) => {
-    res.send(req.headers)
-})
-
-/**
- * @swagger
- * /:
- *   get:
- *     description: get hostname and get local ip address
- *     responses:
- *       200:
- *         description: Success
- * 
- */
 app.get('/', (req, res) => {
     res.send('Requester ip = ' + req.ip)
 })
-app.get('/chain', async(req, res) => {
-    const f5 = await axios.get('http://nginx2/api/f5')
-    // const f5 = 'hello'
-    res.send('aa')
-})
-app.get('/method', async(req, res) => {
-    const apiKey = '0ae3b91ad3msh0092950f607a17cp1204ffjsn70f76814dba4'
-    const hostAPI = 'imdb8.p.rapidapi.com'
-    const baseURL = 'https://imdb8.p.rapidapi.com'
-    const config = {
-        method: 'get',
-        url: `${baseURL}/auto-complete`,
-        params: { q: this.search },
-        headers: {
-            'Content-Type': 'Application/json',
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': hostAPI
-        },
-    }
-    await axios(config)
-        .then(() => {
-            res.send('success')
-            // console.log(response.data.d)
-        })
-        .catch((e) => {
-            console.log(e.message)
-        })
-})
 
-app.get('/test/url', (req, res) => {
-    res.send(req.url)
-})
-
-/**
- * @swagger
- * /f5:
- *   get:
- *     description: Get some F5 products
- *     responses:
- *       200:
- *         description: Success
- * 
- */
 app.get('/f5', async (req, res) => {
     res.json(products)
 })
@@ -144,21 +48,6 @@ app.get('/loadtest', async (req, res) => {
     });
     
 })
-
-app.post('/compare', (req, res) => {
-    console.log(req.body)
-    const num1 = req.body.num1
-    const num2 = req.body.num2
-
-    if (num1 > num2) {
-        res.send(`${num1} more than ${num2}`)
-    }
-    else {
-        res.send(`${num2} more than ${num1}`)
-    }
-
-})
-
 
 httpServer.listen(8080, () => {
     console.log(`Listening at http://localhost:8080`);
